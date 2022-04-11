50eeafe512d3f9f8b6ec3d23f0e82cd6e9522aaa
ant_grey <odintheoneeye@protonmail.com>
2022-04-11 18:33:52 +0200

    refactoring

--- src/App.js
+++ src/App.js
@@ -3,18 +3,9 @@ import './App.css';
 import Navbar from './components/navbar/Navbar';
 import Hero from './components/hero/Hero';
 import Card from './components/card/Card';
-// import zaferesImage from './assets/images/zaferes.png';
 import CardData from './data';
 
 function App() {
-  // const cardData = {
-  //   image: zaferesImage,
-  //   rating: 5.0,
-  //   reviewCount: 6,
-  //   country: 'USA',
-  //   title: 'Life lessons with Katie Zaferes',
-  //   price: 136,
-  // };
   return (
     <div className="App">
       <Navbar />
